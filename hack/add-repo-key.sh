7a9c511938259f43e364d7890f04621f95413180
Maarten den Braber <m@mdbraber.com>
2020-05-27 01:25:40 +0200

    Update add-repo script

--- hack/add-repo-key.sh
+++ hack/add-repo-key.sh
@@ -1,27 +1,34 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# Credit: billimek@github
-# https://github.com/billimek/k8s-gitops/blob/master/setup/add-repo-key.sh
-
+# Script Source: https://github.com/billimek
 # This script will, for the current repo:
 # 1. delete any existing repo keys
 # 2. add a new repo key from the input passed-in
 
-#
-# @CHANGEME - Update to your GitHub Username
-#
-github_username=onedr0p
-
 if [ ! -f "$HOME/.secret/github_access_token" ]; then
-    echo "$HOME/.secret/github_access_token needs to exist with a GitHub personal access token"
-    exit 1
+  echo "$HOME/.secret/github_access_token needs to exist with a GitHub personal access token"
+  exit 1
 fi
 
 if [ -z "$1" ]; then
-    echo "This script requires an ssh-key passed in as an argument"
-    exit 1
+  echo "This script requires a github username passed in as an argument"
+  exit 1
 else
-    KEY="$1"
+  github_username="$1"
+fi
+
+if [ -z "$2" ]; then
+  echo "This script requires an ssh-key passed in as an argument"
+  exit 1
+else
+  KEY="$2"
+fi
+
+if [ -z "$3" ]; then
+  echo "This script requires a repo name passed in as an argument"
+  exit 1
+else
+  reponame="$3"
 fi
 
 need() {
@@ -32,48 +39,33 @@ need "git"
 need "curl"
 need "jq"
 
-github_access_token=$(cat "${HOME}"/.secret/github_access_token)
+github_access_token=$(cat "$HOME"/.secret/github_access_token)
 
-rp=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+repo_id="github-deploy-$reponame.github.com"
 
-if [ "$?" -eq 0 ] && [ "$rp" = "true" ]; then
-    url="$(git config --get remote.origin.url)"
-    repo=${url##*/}
-    reponame=${repo%%.*}
-
-    repo_id="github-deploy-$reponame.github.com"
-
-    # delete all existing deploy keys
-    curl \
-        -H"Authorization: token $github_access_token"\
-        https://api.github.com/repos/"$github_username"/"$reponame"/keys 2>/dev/null\
-        | jq '.[] | .id ' | \
-        while read _id; do
-            echo "- deploy key: $_id"
-            curl \
-                -X "DELETE"\
-                -H"Authorization: token $github_access_token"\
-                https://api.github.com/repos/"$github_username"/"$reponame"/keys/"$_id" 2>/dev/null
-        done
-
-    # add the keyfile to github
-    echo
-    echo "+ deploy key:"
-    echo -n ">> "
-    {
+# delete all existing deploy keys
+curl \
+    -H"Authorization: token $github_access_token"\
+    https://api.github.com/repos/"$github_username"/"$reponame"/keys 2>/dev/null\
+    | jq '.[] | .id ' | \
+    while read _id; do
+        echo "- deploy key: $_id"
         curl \
-            -i\
+            -X "DELETE"\
             -H"Authorization: token $github_access_token"\
-            --data @- https://api.github.com/repos/"$github_username"/"$reponame"/keys << EOF
-    {
-        "title" : "$repo_id $(date)",
-        "key" : "$KEY",
-        "read_only" : false
-    }
+            https://api.github.com/repos/"$github_username"/"$reponame"/keys/"$_id" 2>/dev/null
+    done
+# add the keyfile to github
+echo
+echo "+ deploy key:"
+echo -n ">> "
+curl \
+    -i\
+    -H"Authorization: token $github_access_token"\
+    --data @- https://api.github.com/repos/"$github_username"/"$reponame"/keys << EOF
+{
+"title" : "$repo_id $(date)",
+"key" : "$KEY",
+"read_only" : false
+}
 EOF
-    } 2>/dev/null | head -1 # status code should be 201
-
-else
-    echo 'Not a git repository'
-    exit
-fi
